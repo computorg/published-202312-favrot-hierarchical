5af4ef610964d323a9854dde7f473445cb4d3cfd
Julien Chiquet <julien.chiquet@inrae.fr>
2025-06-24 14:37:59 +0200

    README + more sys dep

--- setup-env-ci.sh
+++ setup-env-ci.sh
@@ -1,1 +1,1 @@
-sudo apt install -y libcurl4-openssl-dev libglpk-dev libpng-dev libx11-dev pandoc jags libfontconfig1-dev
+sudo apt install -y libcurl4-openssl-dev libglpk-dev libpng-dev libx11-dev pandoc jags libfontconfig1-dev libharfbuzz-dev libfribidi-dev
